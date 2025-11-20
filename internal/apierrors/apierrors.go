67ea0b5cf38668e746eb5ffd40891e6ca2f84dca
peshkanavalnogo <playvladosik@gmail.com>
2025-11-20 23:10:21 +0300

    feat/error-response: add apierrors package that helps with error response

---
+++ internal/apierrors/apierrors.go
@@ -0,0 +1,37 @@
+package apierrors
+
+import (
+	"log/slog"
+	"net/http"
+	"runtime/debug"
+
+	"github.com/vladgrskkh/pr_service/pkg/helpers/json"
+)
+
+func logError(logger *slog.Logger, r *http.Request, err error) {
+	logger.Error(err.Error(),
+		slog.String("request_method", r.Method),
+		slog.String("request_url", r.URL.String()),
+		slog.String("trace", string(debug.Stack())))
+}
+
+// errorResponse writes a JSON response with a provided status code and message
+// to the http.ResponseWriter.
+func ErrorResponse(logger *slog.Logger, w http.ResponseWriter, r *http.Request, status int, message interface{}) {
+	data := json.Envelope{
+		"error": message,
+	}
+
+	err := json.Write(w, status, data, nil)
+	if err != nil {
+		logError(logger, r, err)
+		w.WriteHeader(500)
+	}
+}
+
+func ServerErrorResponse(logger *slog.Logger, w http.ResponseWriter, r *http.Request, err error) {
+	logError(logger, r, err)
+
+	message := "server encountered a problem and could not process your request"
+	ErrorResponse(logger, w, r, http.StatusInternalServerError, message)
+}
--- internal/handlers/healthcheck/healthcheck.go
+++ internal/handlers/healthcheck/healthcheck.go
@@ -4,20 +4,9 @@ import (
 	"log/slog"
 	"net/http"
 
+	"github.com/vladgrskkh/pr_service/internal/apierrors"
 	"github.com/vladgrskkh/pr_service/pkg/helpers/json"
 )
-
-func errorResponse(logger *slog.Logger, w http.ResponseWriter, status int, message interface{}) {
-	data := json.Envelope{
-		"error": message,
-	}
-
-	err := json.Write(w, status, data, nil)
-	if err != nil {
-		logger.Error("error writing json", slog.Any("error", err))
-		w.WriteHeader(500)
-	}
-}
 
 func New(logger *slog.Logger, env, version string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -30,7 +19,7 @@ func New(logger *slog.Logger, env, version string) http.HandlerFunc {
 		err := json.Write(w, http.StatusOK, data, nil)
 		if err != nil {
 			logger.Error("error writing json", slog.Any("error", err))
-			errorResponse(logger, w, http.StatusInternalServerError, "server encountered a problem and could not process your request")
+			apierrors.ServerErrorResponse(logger, w, r, err)
 		}
 	}
 }
